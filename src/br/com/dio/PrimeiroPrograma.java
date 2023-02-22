7b85e957acd3a1ca111663d3df9f7c1aa28b9c66
Gabriel Rodrigues <rodriguesgomesgabriel@gmail.com>
2023-02-22 16:16:51 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		Gato gato = new Gato();
+		
+		
+		//int a = 2;
+		//int b = 3;
+		//System.out.println("Oi " + (a+b));
+		
+		
+	}
+
+}
+
+class Livro{
+	private String nome;
+	private String npag;
+}
